1531205b94545a3288c0967923626b5969d57d0a
Thomas <thomas@artisteeq.be>
2026-03-31 10:33:08 +0200

    possible fixes for 404

--- wp-content/themes/Avada-Child/header.php
+++ wp-content/themes/Avada-Child/header.php
@@ -20,28 +20,28 @@ $$$$$$$  |\$$$$$$$\   \$  /   \$$$$$$$\ $$ |\$$$$$$  |$$$$$$$  |\$$$$$$$\ \$$$$$
  */
 
 // Do not allow directly accessing this file.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit( 'Direct script access denied.' );
+if (!defined('ABSPATH')) {
+    exit('Direct script access denied.');
 }
 ?>
 <!DOCTYPE html>
 <html class="<?php avada_the_html_class(); ?>" <?php language_attributes(); ?>>
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<?php Avada()->head->the_viewport(); ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <?php Avada()->head->the_viewport(); ?>
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
-	<?php
-	/**
-	 * The setting below is not sanitized.
-	 * In order to be able to take advantage of this,
-	 * a user would have to gain access to the database
-	 * in which case this is the least of your worries.
-	 */
-	echo apply_filters( 'avada_space_head', Avada()->settings->get( 'space_head' ) ); // phpcs:ignore WordPress.Security.EscapeOutput
-	?>
+    <?php
+    /**
+     * The setting below is not sanitized.
+     * In order to be able to take advantage of this,
+     * a user would have to gain access to the database
+     * in which case this is the least of your worries.
+     */
+    echo apply_filters('avada_space_head', Avada()->settings->get('space_head')); // phpcs:ignore WordPress.Security.EscapeOutput
+    ?>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
@@ -50,7 +50,11 @@ if ( ! defined( 'ABSPATH' ) ) {
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-25Q1V1LBQZ"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'G-25Q1V1LBQZ');
@@ -58,62 +62,61 @@ if ( ! defined( 'ABSPATH' ) ) {
 </head>
 
 <?php
-$object_id      = get_queried_object_id();
-$c_page_id      = Avada()->fusion_library->get_page_id();
-$wrapper_class  = 'fusion-wrapper';
-$wrapper_class .= ( is_page_template( 'blank.php' ) ) ? ' wrapper_blank' : '';
+$object_id = get_queried_object_id();
+$c_page_id = Avada()->fusion_library->get_page_id();
+$wrapper_class = 'fusion-wrapper';
+$wrapper_class .= (is_page_template('blank.php')) ? ' wrapper_blank' : '';
 ?>
-<body <?php body_class(); ?> <?php fusion_element_attributes( 'body' ); ?>>
-	<?php do_action( 'avada_before_body_content' ); ?>
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'Avada' ); ?></a>
+<body <?php body_class(); ?> <?php fusion_element_attributes('body'); ?>>
+<?php do_action('avada_before_body_content'); ?>
+<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'Avada'); ?></a>
 
-	<div id="boxed-wrapper">
-        <div class="progress-wrap hover-target">
-            <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
-            </svg>
-        </div>
-        <?php echo do_shortcode('[ae_popup]'); ?>
-        <div class="fusion-sides-frame"></div>
-		<div id="wrapper" class="<?php echo esc_attr( $wrapper_class ); ?>">
-			<div id="home" style="position:relative;top:-1px;"></div>
-			<?php if ( has_action( 'avada_render_header' ) ) : ?>
-				<?php do_action( 'avada_render_header' ); ?>
-			<?php else : ?>
+<div id="boxed-wrapper">
+    <div class="progress-wrap hover-target">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
+        </svg>
+    </div>
+    <?php echo do_shortcode('[ae_popup]'); ?>
+    <div class="fusion-sides-frame"></div>
+    <div id="wrapper" class="<?php echo esc_attr($wrapper_class); ?>">
+        <div id="home" style="position:relative;top:-1px;"></div>
+        <?php if (has_action('avada_render_header')) : ?>
+            <?php do_action('avada_render_header'); ?>
+        <?php else : ?>
 
-				<?php avada_header_template( 'below', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
-				<?php if ( 'left' === fusion_get_option( 'header_position' ) || 'right' === fusion_get_option( 'header_position' ) ) : ?>
-					<?php avada_side_header(); ?>
-				<?php endif; ?>
+            <?php avada_header_template('below', (is_archive() || Avada_Helper::bbp_is_topic_tag()) && !(class_exists('WooCommerce') && is_shop())); ?>
+            <?php if ('left' === fusion_get_option('header_position') || 'right' === fusion_get_option('header_position')) : ?>
+                <?php avada_side_header(); ?>
+            <?php endif; ?>
 
-				<?php avada_sliders_container(); ?>
+            <?php avada_sliders_container(); ?>
 
-				<?php avada_header_template( 'above', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+            <?php avada_header_template('above', (is_archive() || Avada_Helper::bbp_is_topic_tag()) && !(class_exists('WooCommerce') && is_shop())); ?>
 
-			<?php endif; ?>
+        <?php endif; ?>
 
-			<?php avada_current_page_title_bar( $c_page_id ); ?>
+        <?php avada_current_page_title_bar($c_page_id); ?>
 
-			<?php
-			$row_css    = '';
-			$main_class = '';
+        <?php
+        $row_css = '';
+        $main_class = '';
 
-			if ( apply_filters( 'fusion_is_hundred_percent_template', false, $c_page_id ) ) {
-				$row_css    = 'max-width:100%;';
-				$main_class = 'width-100';
-			}
+        if (apply_filters('fusion_is_hundred_percent_template', false, $c_page_id)) {
+            $row_css = 'max-width:100%;';
+            $main_class = 'width-100';
+        }
 
-			if ( fusion_get_option( 'content_bg_full' ) && 'no' !== fusion_get_option( 'content_bg_full' ) ) {
-				$main_class .= ' full-bg';
-			}
-			do_action( 'avada_before_main_container' );
-			?>
-            <?php
-            if ( is_single() || is_404() ) {
-                $main_class .= ' width-100';
-                $row_css = 'max-width:100%;';
-            }
-            ?>
+        if (fusion_get_option('content_bg_full') && 'no' !== fusion_get_option('content_bg_full')) {
+            $main_class .= ' full-bg';
+        }
 
-            <main id="main" class="clearfix <?php echo esc_attr( trim( $main_class ) ); ?>">
-                <div class="fusion-row" style="<?php echo esc_attr( $row_css ); ?>">
+        if (is_single()) {
+            $main_class .= ' width-100';
+            $row_css = 'max-width:100%;';
+        }
+
+        do_action('avada_before_main_container');
+        ?>
+        <main id="main" class="clearfix <?php echo esc_attr(trim($main_class)); ?>">
+            <div class="fusion-row" style="<?php echo esc_attr($row_css); ?>">
